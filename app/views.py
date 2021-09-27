cfb4d323f64b5dcef07e45bc49a04bbece1d4549
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-09-27 02:44:29 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from app.teams_info import Team, TEAMS
 
 def all_teams(request: HttpRequest):
     return render(request, "teams.html")
 
-def management(request: HttpRequest):
-    return render(request, "manage.html")
-    
-def procurement(request: HttpRequest):
-    return render(request, "procure.html")
 
-def documentation(request: HttpRequest):
-    return render(request, "document.html")
-
-def community(request: HttpRequest):
-    return render(request, "commune.html")
+def which_team(request: HttpRequest, title: str):
+    for team in TEAMS:
+        if title == team.title:
+            return render(request, "team.html", {"name": team.name, "team_info": team.info, "members": team.members})
